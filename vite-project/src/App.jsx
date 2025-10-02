ab47d98527831b64fdefe8635bbea10a8bd4b371
aleksandertap <aleksander.tapsi@voco.ee>
2025-10-02 13:08:07 +0300

    test

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { getData } from './data/data'
 
 function App() {
-  
+  const data = getData()
 
   return (
     <>
-      <p>heel</p>
+      {data.map(item => (
+        <li key={item.id}>
+          {item.name}
+        </li>
+      ))}
     </>
   )
 }
