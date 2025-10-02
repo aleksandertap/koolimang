977f4afc568acfd9a7a1649af0e14d36ca8e07b8
aleksandertap <aleksander.tapsi@voco.ee>
2025-10-02 15:33:48 +0300

    test

--- src/components/questions/QuestionCard.jsx
+++ src/components/questions/QuestionCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import "./QuestionCard.css"
 
 const QuestionCard = (props) => {
     const {text} = props
 
   return (
-    <div>
+    <div className='cardContainer'>
         <p>{text}</p>
     </div>
   )
--- src/data/data.js
+++ src/data/data.js
@@ -1,53 +1,39 @@
 const data = [
-    {
-        id: 1,
-        name: "Ehituskool",
-        erialad: [
-            {icon: "", title: "", value: ""}
-        ],
-    },
-    {
-        id: 2,
-        name: "Turismikool",
-        eriala: [
-            {icon: "", title: "", value: ""}
-        ]
-    },
-    {
-        id: 3,
-        name: "Toidukool",
-        eriala: [
-            {icon: "", title: "", value: ""}
-        ]
-    },
-    {
-        id: 4,
-        name: "Ärikool",
-        eriala: [
-            {icon: "", title: "", value: ""}
-        ]
-    },
-    {
-        id: 5,
-        name: "Ilukool",
-        eriala: [
-            {icon: "", title: "", value: ""}
-        ]
-    },
-    {
-        id: 6,
-        name: "Tehnikakool",
-        eriala: [
-            {icon: "", title: "", value: ""}
-        ]
-    },
-    {
-        id: 7,
-        name: "IT-Kool",
-        eriala: [
-            {icon: "", title: "", value: ""}
-        ]
-    },
-]
+  {
+    id: 1,
+    name: "Ehituskool",
+    erialad: [{ icon: "", title: "", value: "" }],
+  },
+  {
+    id: 2,
+    name: "Turismikool",
+    eriala: [{ icon: "", title: "", value: "" }],
+  },
+  {
+    id: 3,
+    name: "Toidukool",
+    eriala: [{ icon: "", title: "", value: "" }],
+  },
+  {
+    id: 4,
+    name: "Ärikool",
+    eriala: [{ icon: "", title: "", value: "" }],
+  },
+  {
+    id: 5,
+    name: "Ilukool",
+    eriala: [{ icon: "", title: "", value: "" }],
+  },
+  {
+    id: 6,
+    name: "Tehnikakool",
+    eriala: [{ icon: "", title: "", value: "" }],
+  },
+  {
+    id: 7,
+    name: "IT-Kool",
+    eriala: [{ icon: "", title: "", value: "" }],
+  },
+];
 
-export const getData = () => data
+export const getData = () => data;
