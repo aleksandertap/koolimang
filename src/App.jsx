424087f4d8837bac7263e30296095cd1b4d98579
aleksandertap <aleksander.tapsi@voco.ee>
2025-10-02 17:22:37 +0300

    small change before test

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ function App() {
   return (
     <>
       <MainMenu></MainMenu>
-      <Table/>
     </>
   )
 }
--- src/data/data.js
+++ src/data/data.js
@@ -2,36 +2,43 @@ const data = [
   {
     id: 1,
     name: "Ehituskool",
+    description: "",
     erialad: [{ icon: "", title: "", value: "" }],
   },
   {
     id: 2,
     name: "Turismikool",
+    description:"",
     eriala: [{ icon: "", title: "", value: "" }],
   },
   {
     id: 3,
     name: "Toidukool",
+    description:"",
     eriala: [{ icon: "", title: "", value: "" }],
   },
   {
     id: 4,
     name: "Ärikool",
+    description: "",
     eriala: [{ icon: "", title: "", value: "" }],
   },
   {
     id: 5,
     name: "Ilukool",
+    description: "",
     eriala: [{ icon: "", title: "", value: "" }],
   },
   {
     id: 6,
     name: "Tehnikakool",
+    description:"",
     eriala: [{ icon: "", title: "", value: "" }],
   },
   {
     id: 7,
     name: "IT-Kool",
+    description:"",
     eriala: [{ icon: "", title: "", value: "" }],
   },
 ];
