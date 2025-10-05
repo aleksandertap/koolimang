5fda2f65d285731f81c01d48f519b6e804977719
aleksandertap <aleksander.tapsi@voco.ee>
2025-10-05 13:32:46 +0300

    testing pictures

--- src/data/data.js
+++ src/data/data.js
@@ -1,10 +1,33 @@
+import ehituskoolIcon from '../assets/img/icons/ehituskool.png';
+import ehituskoolPicture from '../assets/img/pictures/ehituskool.png';
+
+import itkoolIcon from '../assets/img/icons/itkool.png';
+import itkoolPicture from '../assets/img/pictures/itkool.png';
+
+import toidukoolIcon from '../assets/img/icons/toidukool.png';
+import toidukoolPicture from '../assets/img/pictures/toidukool.png';
+
+import arikoolIcon from '../assets/img/icons/arikool.png';
+import arikoolPicture from '../assets/img/pictures/arikool.png';
+
+import ilukoolIcon from '../assets/img/icons/ilukool.png';
+import ilukoolPicture from '../assets/img/pictures/ilukool.png';
+
+import tehnikakoolIcon from '../assets/img/icons/tehnikakool.png';
+import tehnikakoolPicture from '../assets/img/pictures/tehnikakool.png';
+
+import turismikoolIcon from '../assets/img/icons/turismikool.png';
+import turismikoolPicture from '../assets/img/pictures/turismikool.png';
+
+import poleValdkondaPicture from '../assets/img/pictures/pole_valdkonda.png';
+
 const data = [
   {
     id: 1,
     name: "Ehituskool",
     description: "",
-    icon: "../assets/img/icons/ehituskool.png",
-    picture: "../assets/img/pictures/ehituskool.png",
+    icon: ehituskoolIcon,
+    picture: ehituskoolPicture,
     suggestion: [
       "Olen kannatlik, täpne ja naudin, kui saan oma kätega midagi nullist üles ehitada.", 
       "Mind huvitab hoonete, ruumide, konstruktsioonide ja mööbli loomine ja taastamine.",
@@ -15,8 +38,8 @@ const data = [
     id: 2,
     name: "IT-Kool",
     description: "",
-    icon: "../assets/img/icons/itkool.png",
-    picture: "../assets/img/pictures/itkool.png",
+    icon: itkoolIcon,
+    picture: itkoolPicture,
     suggestion: [
       "Olen analüütiline, uudishimulik ja naudin, kui saan keerulisi probleeme lahendada.",
       "Mind huvitab programmeerimine, süsteemide töö ja digimaailma kujundamine.",
@@ -27,8 +50,8 @@ const data = [
     id: 3,
     name: "Toidukool",
     description: "",
-    icon: "../assets/img/icons/toidukool.png",
-    picture: "../assets/img/pictures/toidukool.png",
+    icon: toidukoolIcon,
+    picture: toidukoolPicture,
     suggestion: [
       "Olen hoolikas ja tähelepanelik, mulle meeldib pakkuda inimestele meeldivaid elamusi läbi toidu ja teeninduse.",
       "Mind huvitab nii toidu valmistamine kui ka selle serveerimine ja külalislahke teenindus.",
@@ -39,8 +62,8 @@ const data = [
     id: 4,
     name: "Ärikool",
     description: "",
-    icon: "../assets/img/icons/arikool.png",
-    picture: "../assets/img/pictures/arikool.png",
+    icon: arikoolIcon,
+    picture: arikoolPicture,
     suggestion: [
       "Olen organiseeritud, süsteemne ja naudin, kui saan protsesse sujuvamaks muuta.",
       "Mind huvitab ettevõtete toimimine, töö korraldamine ja inimeste juhtimine.",
@@ -51,8 +74,8 @@ const data = [
     id: 5,
     name: "Ilukool",
     description: "",
-    icon: "../assets/img/icons/ilukool.png",
-    picture: "../assets/img/pictures/ilukool.png",
+    icon: ilukoolIcon,
+    picture: ilukoolPicture,
     suggestion: [
       "Olen loominguline ja väärtustan ilu ning täpsust väikestes detailides.",
       "Mind huvitab inimeste välimuse, heaolu ja enesekindluse kujundamine.",
@@ -63,8 +86,8 @@ const data = [
     id: 6,
     name: "Tehnikakool",
     description: "",
-    icon: "../assets/img/icons/tehnikakool.png",
-    picture: "../assets/img/pictures/tehnikakool.png",
+    icon: tehnikakoolIcon,
+    picture: tehnikakoolPicture,
     suggestion: [
       "Olen loogilise mõtlemisega, täpne ja praktiline ning naudin olukordi, kus saan lahendada tehnilisi ülesandeid ja näidata enda käelist osavust.",
       "Mind huvitab, kuidas masinaid, seadmeid ja süsteeme nii luua, paremaks muuta kui ka hooldada.",
@@ -76,8 +99,8 @@ const data = [
     id: 7,
     name: "Turismikool",
     description: "",
-    icon: "../assets/img/icons/turismikool.png",
-    picture: "../assets/img/pictures/turismikool.png",
+    icon: turismikoolIcon,
+    picture: turismikoolPicture,
     suggestion: [
       "Olen avatud ja rõõmsameelne, naudin suhtlemist väga erinevate inimestega.",
       "Mind huvitab puhtuse, korra ja hubasuse loomine, et inimestel oleks meeldiv viibida erinevates keskkondades.",
@@ -89,7 +112,7 @@ const data = [
     name: "Oh ei! Me ei leidnud sulle sobivat osakonda!",
     description: "",
     icon: null,
-    picture: "../assets/img/pictures/pole_valdkonda.png",
+    picture: poleValdkondaPicture,
     suggestion: [
       "Olen pigem teoreetilise meelega ja eelistan akadeemilisi aineid praktilistele oskustele.", 
       "Mind köidavad teemad, mis eeldavad rohkem teoreetilist süvenemist kui praktilist tegutsemist.",
